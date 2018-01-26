ea44b03adc809205745272fcd709e951d85c5342
Neil Cudden <ncudden@sbcglobal.net>
2018-01-26 14:32:41 -0700

    Load HTML & JSON using JS methods 1 & 2 finished 1-26-2018

--- JS/AjaxTest.js
+++ JS/AjaxTest.js
@@ -6,14 +6,43 @@ $(function(){
     
     $('#titleBox').on('click',function(){
         
-    // Begin Method 1: Load Using JavaScript
         var xhr = new XMLHttpRequest();
-        xhr.onload = function(){
-            document.getElementById('insert').innerHTML = xhr.responseText;
-        };
-        xhr.open('GET', 'HTML/addMyResumeObjective.html', true);
+        
+        // Begin Method 1: Load HTML Data Using JavaScript
+            /*
+            xhr.onload = function(){
+                document.getElementById('insert').innerHTML = xhr.responseText;
+            };
+            xhr.open('GET', 'HTML/addMyResumeObjective.html', true);
+            */
+        // End Method 1
+        
+        // Begin Method 2: Load JSON Data Using JavaScript
+            xhr.onload = function(){
+                responseObject = JSON.parse(xhr.responseText);
+                var newContent = '';
+                newContent += '<div class="' + responseObject.styles + '" id="' + responseObject.id + '">';
+                newContent += '<p>' + responseObject.myMessage + '</p>';
+                newContent += '</div>';
+                document.getElementById('insert').innerHTML = newContent;
+            };
+            xhr.open('GET', 'JSON/addMyResumeObjective.json', true);
+        // End Method 2
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         xhr.send(null);
-    // End Method 1
+    
         
     });
 
