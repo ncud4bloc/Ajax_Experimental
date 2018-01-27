44fcf30739408b38c8ac5fc7dae8f7911adfb2c8
Neil Cudden <ncudden@sbcglobal.net>
2018-01-26 17:27:45 -0700

    Load HTML & JSON using JS methods 1 & 2 finished 1-26-2018

--- JS/AjaxTest.js
+++ JS/AjaxTest.js
@@ -1,23 +1,21 @@
-/* -----  Functions  ----- */
-
-/* -----  Function Calls  ----- */
-
 $(function(){
     
     $('#titleBox').on('click',function(){
         
-        var xhr = new XMLHttpRequest();
+    // Used by Methods 1 & 2
+        /* var xhr = new XMLHttpRequest(); */
         
-        // Begin Method 1: Load HTML Data Using JavaScript
+    // Begin Method 1: Load HTML Data Using JavaScript
             /*
             xhr.onload = function(){
                 document.getElementById('insert').innerHTML = xhr.responseText;
             };
             xhr.open('GET', 'HTML/addMyResumeObjective.html', true);
             */
-        // End Method 1
+    // End Method 1
         
-        // Begin Method 2: Load JSON Data Using JavaScript
+    // Begin Method 2: Load JSON Data Using JavaScript
+            /*
             xhr.onload = function(){
                 responseObject = JSON.parse(xhr.responseText);
                 var newContent = '';
@@ -27,21 +25,45 @@ $(function(){
                 document.getElementById('insert').innerHTML = newContent;
             };
             xhr.open('GET', 'JSON/addMyResumeObjective.json', true);
-        // End Method 2
+            */
+    // End Method 2
+        
+    // Begin Method 3A: Load HTML Data Using jQuery .load() method
+            /*
+            $('#insert').load("HTML/ducati916.html" + ' #para2').hide().fadeIn(3000);
+            */
+    // End Method 3A
+        
+    // Begin Method 3B: Load HTML Data Using jQuery $.ajax() method
+        
+            $.ajax({
+                type: "GET",
+                url: "HTML/ducati916.html",
+                timeout: 2000,
+                beforeSend: function(){
+                    $('#insert').append('<div id="load">Loading....</div>');
+                    console.log('Loading....');
+                },
+                complete: function(){
+                    $('#load').remove();
+                    console.log('Loading Complete');
+                },
+                success: function(data){
+                    $('#insert').html($(data).find('#para1')).hide().fadeIn(3000);    //jQuery .find() doesn't work
+                    //$('#insert').html($(data)).hide().fadeIn(3000);
+                    console.log('Data Transfer Successful');        
+                },
+                error: function(){
+                    $('#insert').html('<div id="eMessage">Error.....file did not load correctly</div>');
+                    console.log('Error Getting Data');
+                }
+            });
+        
+    // End Method 3B
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        xhr.send(null);
+    // Used by Methods 1 & 2
+        /* xhr.send(null); */
     
         
     });
